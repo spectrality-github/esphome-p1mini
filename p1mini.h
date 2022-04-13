9a770625592f6bfb9a15291f9879bf69a247fdde
Beaky2000@github.com <johnnyjo@gmail.com>
2022-04-13 20:11:17 +0200

    First running version.

--- p1mini.h
+++ p1mini.h
@@ -1,9 +1,11 @@
 //-------------------------------------------------------------------------------------
 // ESPHome P1 Electricity Meter custom sensor
+// Copyright 2022 Johnny Johansson
 // Copyright 2020 Pär Svanström
 // 
 // History
 //  0.1.0 2020-11-05:   Initial release
+//  0.2.0 2022-04-13:   Major rewrite
 //
 // MIT License
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
@@ -20,295 +22,234 @@
 
 #include "esphome.h"
 
-#define BUF_SIZE 60
-
-class ParsedMessage {
-  public:
-    double cumulativeActiveImport;
-    double cumulativeActiveExport;
-
-    double cumulativeReactiveImport;
-    double cumulativeReactiveExport;
-
-    double momentaryActiveImport;
-    double momentaryActiveExport;
-
-    double momentaryReactiveImport;
-    double momentaryReactiveExport;
-
-    double momentaryActiveImportL1;
-    double momentaryActiveExportL1;
-
-    double momentaryActiveImportL2;
-    double momentaryActiveExportL2;
-
-    double momentaryActiveImportL3;
-    double momentaryActiveExportL3;
-
-    double momentaryReactiveImportL1;
-    double momentaryReactiveExportL1;
-
-    double momentaryReactiveImportL2;
-    double momentaryReactiveExportL2;
-
-    double momentaryReactiveImportL3;
-    double momentaryReactiveExportL3;
-
-    double voltageL1;
-    double voltageL2;
-    double voltageL3;
-
-    double currentL1;
-    double currentL2;
-    double currentL3;
-
-    bool crcOk = false;
-};
-
 class P1Reader : public Component, public UARTDevice {
-  const char* DELIMITERS = "()*:";
-  const char* DATA_ID = "1-0";
-  char buffer[BUF_SIZE];
+public:
 
+  // Call from a lambda in the yaml file to set up each sensor.
+  Sensor *AddSensor(int major, int minor, int micro)
+  {
+    m_sensor_list = new SensorListItem(m_sensor_list, OBIS(major, minor, micro));
+    return m_sensor_list->GetSensor();
+  }
+
+  P1Reader(unsigned int minimum_period_ms,
+           UARTComponent *parent,
+           esphome::gpio::GPIOSwitch * CTS_switch,
+           esphome::gpio::GPIOSwitch * status_switch = nullptr)
+    : UARTDevice(parent) 
+    , m_minimum_period_ms{ minimum_period_ms }
+    , m_CTS_switch{ CTS_switch }
+    , m_status_switch{ status_switch }
+    {}
+
+private:
+  unsigned long const m_minimum_period_ms{ 0 };
+  unsigned long m_period_start_time;
+
+  // Store the main message as it is beeing received:
+  constexpr static int message_buffer_size{ 2048 };
+  char message_buffer[message_buffer_size];
+  int message_buffer_position{ 0 };
+
+  // Store the CRC part of the message as it is beeing received:
+  constexpr static int crc_buffer_size{ 8 };
+  char crc_buffer[crc_buffer_size];
+  int crc_buffer_position{ 0 };
+
+  // Calculate the CRC while the message is beeing received
+  uint16_t crc{ 0 };
+
+  enum class states {
+    READING_MESSAGE,
+    READING_CRC,
+    PROCESSING,
+    WAITING
+  };
+  enum states m_state{ states::READING_MESSAGE };
+
+  void ChangeState(enum states new_state)
+  {
+    switch (new_state) {
+    case states::READING_MESSAGE:
+      m_CTS_switch->turn_on();
+      if (m_status_switch != nullptr) m_status_switch->turn_on();
+      crc = message_buffer_position = 0;
+      break;
+    case states::READING_CRC:
+      crc_buffer_position = 0;
+      break;
+    case states::PROCESSING:
+      m_CTS_switch->turn_off();
+      break;
+    case states::WAITING:
+      if (m_state != states::PROCESSING) m_CTS_switch->turn_off();
+      if (m_status_switch != nullptr) m_status_switch->turn_off();
+      break;
+    }
+    m_state = new_state;
+  }
+
+  // Combine the three values defining a sensor into a single unsigned int for easier
+  // handling and comparison
+  static uint32_t OBIS(uint32_t major, uint32_t minor, uint32_t micro)
+  {
+    return (major & 0xfff) << 16 | (minor & 0xff)  << 8 | (micro & 0xff);
+  }
+
+  class SensorListItem {
+    uint32_t m_obisCode;
+    Sensor m_sensor;
+    SensorListItem *m_next{ nullptr };
   public:
-    Sensor *cumulativeActiveImport = new Sensor();
-    Sensor *cumulativeActiveExport = new Sensor();
+    SensorListItem(SensorListItem *next, uint32_t obisCode)
+    : m_obisCode(obisCode)
+    , m_next(next)
+    {}
+    
+    Sensor *GetSensor() { return &m_sensor; }
+    uint32_t GetCode() { return m_obisCode; }
+    SensorListItem *Next() { return m_next; }
+  };
 
-    Sensor *cumulativeReactiveImport = new Sensor();
-    Sensor *cumulativeReactiveExport = new Sensor();
+  // Linked list of all sensors
+  SensorListItem *m_sensor_list { nullptr };
 
-    Sensor *momentaryActiveImport = new Sensor();
-    Sensor *momentaryActiveExport = new Sensor();
+  esphome::gpio::GPIOSwitch * const m_CTS_switch;
+  esphome::gpio::GPIOSwitch * const m_status_switch;
 
-    Sensor *momentaryReactiveImport = new Sensor();
-    Sensor *momentaryReactiveExport = new Sensor();
+public:
 
-    Sensor *momentaryActiveImportL1 = new Sensor();
-    Sensor *momentaryActiveExportL1 = new Sensor();
+  void setup() override 
+  {
+    m_period_start_time = millis();
+    ChangeState(states::READING_MESSAGE);
+  }
 
-    Sensor *momentaryActiveImportL2 = new Sensor();
-    Sensor *momentaryActiveExportL2 = new Sensor();
-
-    Sensor *momentaryActiveImportL3 = new Sensor();
-    Sensor *momentaryActiveExportL3 = new Sensor();
-
-    Sensor *momentaryReactiveImportL1 = new Sensor();
-    Sensor *momentaryReactiveExportL1 = new Sensor();
-
-    Sensor *momentaryReactiveImportL2 = new Sensor();
-    Sensor *momentaryReactiveExportL2 = new Sensor();
-
-    Sensor *momentaryReactiveImportL3 = new Sensor();
-    Sensor *momentaryReactiveExportL3 = new Sensor();
-
-    Sensor *voltageL1 = new Sensor();
-    Sensor *voltageL2 = new Sensor();
-    Sensor *voltageL3 = new Sensor();
-
-    Sensor *currentL1 = new Sensor();
-    Sensor *currentL2 = new Sensor();
-    Sensor *currentL3 = new Sensor();
-
-    P1Reader(UARTComponent *parent) : UARTDevice(parent) {}
-
-    void setup() override { }
-
-    void loop() override {
-      readP1Message();
-    }
-
-  private:
-    uint16_t crc16_update(uint16_t crc, uint8_t a) {
-      int i;
-      crc ^= a;
-      for (i = 0; i < 8; ++i) {
-        if (crc & 1) {
-            crc = (crc >> 1) ^ 0xA001;
-        } else {
-            crc = (crc >> 1);
+  void loop() override {
+    switch (m_state) {
+    case states::READING_MESSAGE:
+    case states::READING_CRC:
+      while (available()) {
+        // While data is available, read it one byte at a time.
+        char const read_byte{ (char)read() }; 
+        if (m_state == states::READING_MESSAGE) {
+          if (message_buffer_position == message_buffer_size) {
+            ESP_LOGW("p1reader", "Message buffer overrun. Resetting.");
+            ChangeState(states::WAITING);
+            while (available()) read();
+            return;
+          }
+          crc16_update(read_byte);
+          if (read_byte == '!') {
+            // The exclamation mark indicates that the main message is complete
+            // and the CRC will come next.
+            message_buffer[message_buffer_position] = '\0';
+            ChangeState(states::READING_CRC);
+          }
+          else message_buffer[message_buffer_position++] = read_byte;
         }
-      }
-      return crc;
-    }
-
-    void parseRow(ParsedMessage* parsed, char* obisCode, char* value) {
-      if (strncmp(obisCode, "1.8.0", 5) == 0) {
-        parsed->cumulativeActiveImport = atof(value);
-
-      } else if (strncmp(obisCode, "2.8.0", 5) == 0) {
-        parsed->cumulativeActiveExport = atof(value);
-
-      } else if (strncmp(obisCode, "3.8.0", 5) == 0) {
-        parsed->cumulativeReactiveImport = atof(value);
-
-      } else if (strncmp(obisCode, "4.8.0", 5) == 0) {
-        parsed->cumulativeReactiveExport = atof(value);
-
-      } else if (strncmp(obisCode, "1.7.0", 5) == 0) {
-        parsed->momentaryActiveImport = atof(value);
-
-      } else if (strncmp(obisCode, "2.7.0", 5) == 0) {
-        parsed->momentaryActiveExport = atof(value);
-
-      } else if (strncmp(obisCode, "3.7.0", 5) == 0) {
-        parsed->momentaryReactiveImport = atof(value);
-
-      } else if (strncmp(obisCode, "4.7.0", 5) == 0) {
-        parsed->momentaryReactiveExport = atof(value);
-
-      } else if (strncmp(obisCode, "21.7.0", 6) == 0) {
-        parsed->momentaryActiveImportL1 = atof(value);
-
-      } else if (strncmp(obisCode, "22.7.0", 6) == 0) {
-        parsed->momentaryActiveExportL1 = atof(value);
-
-      } else if (strncmp(obisCode, "41.7.0", 6) == 0) {
-        parsed->momentaryActiveImportL2 = atof(value);
-
-      } else if (strncmp(obisCode, "42.7.0", 6) == 0) {
-        parsed->momentaryActiveExportL2 = atof(value);
-
-      } else if (strncmp(obisCode, "61.7.0", 6) == 0) {
-        parsed->momentaryActiveImportL3 = atof(value);
-
-      } else if (strncmp(obisCode, "62.7.0", 6) == 0) {
-        parsed->momentaryActiveExportL3 = atof(value);
-
-      } else if (strncmp(obisCode, "23.7.0", 6) == 0) {
-        parsed->momentaryReactiveImportL1 = atof(value);
-
-      } else if (strncmp(obisCode, "24.7.0", 6) == 0) {
-        parsed->momentaryReactiveExportL1 = atof(value);
-
-      } else if (strncmp(obisCode, "43.7.0", 6) == 0) {
-        parsed->momentaryReactiveImportL2 = atof(value);
-
-      } else if (strncmp(obisCode, "44.7.0", 6) == 0) {
-        parsed->momentaryReactiveExportL2 = atof(value);
-
-      } else if (strncmp(obisCode, "63.7.0", 6) == 0) {
-        parsed->momentaryReactiveImportL3 = atof(value);
-
-      } else if (strncmp(obisCode, "64.7.0", 6) == 0) {
-        parsed->momentaryReactiveExportL3 = atof(value);
-
-      } else if (strncmp(obisCode, "32.7.0", 6) == 0) {
-        parsed->voltageL1 = atof(value);
-
-      } else if (strncmp(obisCode, "52.7.0", 6) == 0) {
-        parsed->voltageL2 = atof(value);
-
-      } else if (strncmp(obisCode, "72.7.0", 6) == 0) {
-        parsed->voltageL3 = atof(value);
-
-      } else if (strncmp(obisCode, "31.7.0", 6) == 0) {
-        parsed->currentL1 = atof(value);
-
-      } else if (strncmp(obisCode, "51.7.0", 6) == 0) {
-        parsed->currentL2 = atof(value);
-
-      } else if (strncmp(obisCode, "71.7.0", 6) == 0) {
-        parsed->currentL3 = atof(value);
-      }
-    }
-
-    void publishSensors(ParsedMessage* parsed) {
-      cumulativeActiveImport->publish_state(parsed->cumulativeActiveImport);
-      cumulativeActiveExport->publish_state(parsed->cumulativeActiveExport);
-
-      cumulativeReactiveImport->publish_state(parsed->cumulativeReactiveImport);
-      cumulativeReactiveExport->publish_state(parsed->cumulativeReactiveExport);
-
-      momentaryActiveImport->publish_state(parsed->momentaryActiveImport);
-      momentaryActiveExport->publish_state(parsed->momentaryActiveExport);
-
-      momentaryReactiveImport->publish_state(parsed->momentaryReactiveImport);
-      momentaryReactiveExport->publish_state(parsed->momentaryReactiveExport);
-
-      momentaryActiveImportL1->publish_state(parsed->momentaryActiveImportL1);
-      momentaryActiveExportL1->publish_state(parsed->momentaryActiveExportL1);
-
-      momentaryActiveImportL2->publish_state(parsed->momentaryActiveImportL2);
-      momentaryActiveExportL2->publish_state(parsed->momentaryActiveExportL2);
-
-      momentaryActiveImportL3->publish_state(parsed->momentaryActiveImportL3);
-      momentaryActiveExportL3->publish_state(parsed->momentaryActiveExportL3);
-
-      momentaryReactiveImportL1->publish_state(parsed->momentaryReactiveImportL1);
-      momentaryReactiveExportL1->publish_state(parsed->momentaryReactiveExportL1);
-
-      momentaryReactiveImportL2->publish_state(parsed->momentaryReactiveImportL2);
-      momentaryReactiveExportL2->publish_state(parsed->momentaryReactiveExportL2);
-
-      momentaryReactiveImportL3->publish_state(parsed->momentaryReactiveImportL3);
-      momentaryReactiveExportL3->publish_state(parsed->momentaryReactiveExportL3);
-
-      voltageL1->publish_state(parsed->voltageL1);
-      voltageL2->publish_state(parsed->voltageL2);
-      voltageL3->publish_state(parsed->voltageL3);
-
-      currentL1->publish_state(parsed->currentL1);
-      currentL2->publish_state(parsed->currentL2);
-      currentL3->publish_state(parsed->currentL3);
-    }
-
-    void readP1Message() {
-      if (available()) {
-        uint16_t crc = 0x0000;
-        ParsedMessage parsed = ParsedMessage();
-        bool telegramEnded = false;
-
-        while (available()) {
-          int len = Serial.readBytesUntil('\n', buffer, BUF_SIZE);
-
-          if (len > 0) {
-          	ESP_LOGD("data", "%s", buffer);
-
-            // put newline back as it is required for CRC calculation
-            buffer[len] = '\n';
-            buffer[len + 1] = '\0';
-
-            // if we've reached the CRC checksum, calculate last CRC and compare
-            if (buffer[0] == '!') {
-              crc = crc16_update(crc, buffer[0]);
-              int crcFromMsg = (int) strtol(&buffer[1], NULL, 16);
-              parsed.crcOk = crc == crcFromMsg;
-              ESP_LOGI("crc", "Telegram read. CRC: %04X = %04X. PASS = %s", crc, crcFromMsg, parsed.crcOk ? "YES": "NO");
-              telegramEnded = true;
-
-            // otherwise pass the row through the CRC calculation
-            } else {
-              for (int i = 0; i < len + 1; i++) {
-                crc = crc16_update(crc, buffer[i]);
-              }
+        else { // READING_CRC
+          if (crc_buffer_position == crc_buffer_size) {
+            ESP_LOGW("p1reader", "CRC buffer overrun. Resetting.");
+            ChangeState(states::WAITING);
+            while (available()) read();
+            return;
+          }
+          if (read_byte == '\n') {
+            // The CRC is a single line, so once we reach end of line, we are
+            // ready to verify and process the message.
+            crc_buffer[crc_buffer_position] = '\0';
+            int const crcFromMsg = (int) strtol(crc_buffer, NULL, 16);
+            if (crc != crcFromMsg) {
+              ESP_LOGW("p1reader", "CRC missmatch, calculated %04X != %04X. Message ignored.", crc, crcFromMsg);
+              ESP_LOGD("p1reader", "Buffer:\n%s", message_buffer);
+              ChangeState(states::WAITING);
+              return;
             }
-
-            // if this is a row containing information
-            if (strchr(buffer, '(') != NULL) {
-              char* dataId = strtok(buffer, DELIMITERS);
-              char* obisCode = strtok(NULL, DELIMITERS);
-
-              // ...and this row is a data row, then parse row
-              if (strncmp(DATA_ID, dataId, strlen(DATA_ID)) == 0) {
-                char* value = strtok(NULL, DELIMITERS);
-                char* unit = strtok(NULL, DELIMITERS);
-                parseRow(&parsed, obisCode, value);
-              }
+            else {
+              ChangeState(states::PROCESSING);
+              return;
             }
           }
-          // clean buffer
-          memset(buffer, 0, BUF_SIZE - 1);
-        
-          if (!telegramEnded && !available()) {
-          	// wait for more data
-          	delay(40);
+          else {
+            crc_buffer[crc_buffer_position++] = read_byte;
           }
         }
+      }
+      break;
+    case states::PROCESSING:
+      ParseMessage();
+      ChangeState(states::WAITING);
+      break;
+    case states::WAITING:
+      unsigned long const current_time{ millis() };
+      if (m_minimum_period_ms < current_time - m_period_start_time) {
+        m_period_start_time = current_time;
+        ChangeState(states::READING_MESSAGE);
+      }
+      break;
+    }
+  }
 
-        // if the CRC pass, publish sensor values
-        if (parsed.crcOk) {
-          publishSensors(&parsed);
-        }
+private:
+  void crc16_update(uint8_t a) {
+    int i;
+    crc ^= a;
+    for (i = 0; i < 8; ++i) {
+      if (crc & 1) {
+          crc = (crc >> 1) ^ 0xA001;
+      } else {
+          crc = (crc >> 1);
       }
     }
+  }
+
+
+  // Find the matching sensor in the linked list (or return nullptr
+  // if it does not exist.
+  Sensor *GetSensor(uint32_t obisCode) const
+  {
+    SensorListItem *sensor_list { m_sensor_list };
+    while (sensor_list != nullptr) {
+      if (obisCode == sensor_list->GetCode()) return sensor_list->GetSensor();
+      sensor_list = sensor_list->Next();
+    }
+    return nullptr;
+  }
+
+  // Once a complete message has been received and CRC-verified, this method
+  // will process the contents.
+  void ParseMessage() {
+    char *start_of_line{ message_buffer };
+    while (*start_of_line == '\n' || *start_of_line == '\r') ++start_of_line;
+    
+    // Message will be processed one line at a time:
+    while (*start_of_line != '\0') {
+      char *end_of_line{ start_of_line };
+      while (*end_of_line != '\n' && *end_of_line != '\r' && *end_of_line != '\0') ++end_of_line;
+      bool const last_line{ *end_of_line == '\0' };
+      *end_of_line = '\0';
+
+      if (end_of_line != start_of_line) {
+        int minor{ -1 }, major{ -1 }, micro{ -1 };
+        double value{ -1.0 };
+        if (sscanf(start_of_line, "1-0:%d.%d.%d(%lf", &major, &minor, &micro, &value) != 4) {
+          ESP_LOGD("p1reader", "Could not parse value from line '%s'", start_of_line);
+        }
+        else {
+          uint32_t const obisCode{ OBIS(major, minor, micro) };
+          Sensor *S{ GetSensor(obisCode) };
+          if (S != nullptr) S->publish_state(value);
+          else {
+            ESP_LOGD("p1reader", "No sensor matching: %d.%d.%d (0x%x)", major, minor, micro, obisCode);
+          }
+        }
+      }
+      if (last_line) return;
+      start_of_line = end_of_line + 1;
+      while (*start_of_line == '\n' || *start_of_line == '\r') ++start_of_line;
+    }
+  }
+
+
 };
