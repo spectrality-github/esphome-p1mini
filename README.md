21014d677231c963b4a3012ccf15a9454785c05e
Beaky2000@github.com <johnnyjo@gmail.com>
2022-04-14 21:58:09 +0200

    Misc updates

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 Based on esphome-p1reader, which is an ESPHome custom component for reading P1 data from electricity meters. Designed for Swedish meters that implements the specification defined in the [Swedish Energy Industry Recommendation For Customer Interfaces](https://www.energiforetagen.se/forlag/elnat/branschrekommendation-for-lokalt-kundgranssnitt-for-elmatare/) version 1.3 and above.
 
 Notable differences from esphome-p1reader are:
-* Bla
-* bla
-* bla
+* More frequent updates with configurable update period.
+* No additional components needed. RJ12 cable connects directly to D1Mini (or equivalent)
 
 ## ESPHome version
 The current version in main is tested with ESPHome version `2022.3.0`. Make sure your ESPHome version is up to date if you experience compile problems.
@@ -18,8 +17,6 @@ The current version in main is tested with ESPHome version `2022.3.0`. Make sure
 * [S34U18 (Sanxing SX631)](https://www.vattenfalleldistribution.se/matarbyte/nya-elmataren/) / Vattenfall 
 * [KAIFA MA304H4E](https://reko.nackaenergi.se/elmatarbyte/) / Nacka Energi
 * [KAIFA CL109](https://www.oresundskraft.se/dags-for-matarbyte/) / Öresundskraft
-
-*Note:* There's currently a bug in the E360 firmware, causing it to stop sending out data after a while. Check this comment for more info: https://github.com/psvanstrom/esphome-p1reader/issues/4#issuecomment-810794020
 
 ## Hardware
 I have used a D1 mini clone, but most ESP-based controllers should work as long as you figure out appropriate pins to use. The P1 port on the meter provides 5V up to 250mA which makes it possible to power the circuit directly from the P1 port.
@@ -35,108 +32,19 @@ The circuit is very simple, basically the 5V TX output on the P1 connector is co
 #### Wiring NodeMCU ESP-12
 ![Wiring Diagram](images/wiring.png)
 
-#### Wiring Wemos D1 mini
-![image](https://user-images.githubusercontent.com/5547521/132756141-53941ed7-64f6-4c83-b0b0-6fc7c9634752.png)
-
-### PCB and enclosures
-#### Naesstrom
-[Naesstrom](https://github.com/Naesstrom) has made a nice PCB layout for the P1 reader using a Wemos D1 mini as the controller along with a 3D printable enclosure. 
-
-Check out the PCB here: https://oshwlab.com/Naesstrom/esphome-p1reader and the enclosure here: https://www.thingiverse.com/thing:4961372.
-
-<p float="left">
-    <img src="https://user-images.githubusercontent.com/5547521/128576100-648cd2b7-d728-4d8b-90be-46f7498d8136.png" height="300" />
-    <img src="https://user-images.githubusercontent.com/5547521/132759466-f92bf190-ebaa-401d-bb54-330df5ba3ae0.png" height="300" /> 
-</p>
-
-#### EHjortberg
-[EHjortberg](https://github.com/ehjortberg) has made an equally nice PCB layout based on an ESP07 module along with a 3D printable enclosure. Check it out here: https://github.com/ehjortberg/kicad-p1-port-thingie.
-
-<p float="left">
-  <img src="https://github.com/ehjortberg/kicad-p1-port-thingie/raw/master/images/p1-port-thingie-photo.jpg" width="400">
-</p>
-
-## Optional hardware
-Weigu has designed [SmartyReader P1](http://weigu.lu/microcontroller/smartyReader_P1/index.html) that also can be running with this code and configuration with a few small adaptions.
-
-This hardware runs on ESP8266 Wemos D1 mini pro but with less components.
-
-### Basic steps to run this code on SmartyReader P1:
-
-1. Set the board to Wemos D1 mini pro
-```
-board: d1_mini_pro
-```
-
-2. Adjust the UART section to invert the RX pin (removed TX pin config since it is not used).
-```
-uart:
-    id: uart_bus
-    rx_pin:
-      number: 3
-      inverted: true
-    baud_rate: 115200
-```
-
-~~Note that the inverted flag is only supported in ESPHome beta as of now.~~
-~~Monitor [this PR](https://github.com/esphome/esphome/pull/1727) to follow if it is released to general version.~~
-_inverted flag feature has been added in ESPHome 2021.12.0 released on 11th December 2021._
 
 ## Installation
 Clone the repository and create a companion `secrets.yaml` file with the following fields:
 ```
 wifi_ssid: <your wifi SSID>
 wifi_password: <your wifi password>
-fallback_password: <fallback AP password>
-hass_api_password: <the Home Assistant API password>
-ota_password: <The OTA password>
+p1mini_password: <Your p1mini password (for OTA, API, etc)>
 ```
-Make sure to place the `secrets.yaml` file in the root path of the cloned project. The `fallback_password` and `ota_password` fields can be set to any password before doing the initial upload of the firmware.
+Make sure to place the `secrets.yaml` file in the root path of the cloned project. The `p1mini_password` field can be set to any password before doing the initial upload of the firmware.
 
+Flash ESPHome as usual, just keep the `p1mini.h` file in the same location as `p1mini.yaml` (and `secrets.yaml`). *Don't* connect USB and the P1 port at the same time! If everything works, Home Assistant will autodetect the new integration when you plug it into the P1 port.
 
-
-Prepare the microcontroller with ESPHome before you connect it to the circuit:
-- Install the `esphome` [command line tool](https://esphome.io/guides/getting_started_command_line.html)
-- Plug in the microcontroller to your USB port and run `esphome p1reader.yaml run` to flash the firmware
-- Remove the USB connection and connect the microcontroller to the rest of the circuit and plug it into the P1 port.
-- If everything works, your Home Assistant will now auto detect your new ESPHome integration.
-
-You can check the logs by issuing `esphome p1reader.yaml logs` (or use the super awesome ESPHome dashboard available as a Hass.io add-on or standalone). The logs should output data similar to this every 10 seconds when using `DEBUG` loglevel:
-```
-[18:40:01][D][data:264]: /ELL5\253833635_A
-[18:40:01][D][data:264]:
-[18:40:01][D][data:264]: 0-0:1.0.0(210217184019W)
-[18:40:01][D][data:264]: 1-0:1.8.0(00006678.394*kWh)
-[18:40:01][D][data:264]: 1-0:2.8.0(00000000.000*kWh)
-[18:40:01][D][data:264]: 1-0:3.8.0(00000021.988*kvarh)
-[18:40:01][D][data:264]: 1-0:4.8.0(00001020.971*kvarh)
-[18:40:01][D][data:264]: 1-0:1.7.0(0001.727*kW)
-[18:40:01][D][data:264]: 1-0:2.7.0(0000.000*kW)
-[18:40:01][D][data:264]: 1-0:3.7.0(0000.000*kvar)
-[18:40:01][D][data:264]: 1-0:4.7.0(0000.309*kvar)
-[18:40:01][D][data:264]: 1-0:21.7.0(0001.023*kW)
-[18:40:01][D][data:264]: 1-0:41.7.0(0000.350*kW)
-[18:40:01][D][data:264]: 1-0:61.7.0(0000.353*kW)
-[18:40:01][D][data:264]: 1-0:22.7.0(0000.000*kW)
-[18:40:01][D][data:264]: 1-0:42.7.0(0000.000*kW)
-[18:40:01][D][data:264]: 1-0:62.7.0(0000.000*kW)
-[18:40:01][D][data:264]: 1-0:23.7.0(0000.000*kvar)
-[18:40:01][D][data:264]: 1-0:43.7.0(0000.000*kvar)
-[18:40:01][D][data:264]: 1-0:63.7.0(0000.000*kvar)
-[18:40:01][D][data:264]: 1-0:24.7.0(0000.009*kvar)
-[18:40:01][D][data:264]: 1-0:44.7.0(0000.161*kvar)
-[18:40:01][D][data:264]: 1-0:64.7.0(0000.138*kvar)
-[18:40:01][D][data:264]: 1-0:32.7.0(240.3*V)
-[18:40:01][D][data:264]: 1-0:52.7.0(240.1*V)
-[18:40:01][D][data:264]: 1-0:72.7.0(241.3*V)
-[18:40:01][D][data:264]: 1-0:31.7.0(004.2*A)
-[18:40:01][D][data:264]: 1-0:51.7.0(001.6*A)
-[18:40:01][D][data:264]: 1-0:71.7.0(001.7*A)
-[18:40:01][D][data:264]: !7945
-[18:40:01][I][crc:275]: Telegram read. CRC: 7945 = 7945. PASS = YES
-```
-
-The last row contains the CRC check. If you constantly get invalid CRC there might be something wrong with the serial communication.
+If you do not receive any data, make sure that the P1 port is enabled and try setting the log level to `DEBUG` for more feedback.
 
 ## Technical documentation
 Specification overview:
--- p1mini.h
+++ p1mini.h
@@ -44,20 +44,29 @@ public:
 
 private:
   unsigned long const m_minimum_period_ms{ 0 };
-  unsigned long m_period_start_time;
+  unsigned long m_reading_message_time;
+  unsigned long m_reading_crc_time;
+  unsigned long m_processing_time;
+  unsigned long m_waiting_time;
+  int m_num_message_loops;
+  int m_num_processing_loops;
+  bool m_display_time_stats{ false };
 
   // Store the main message as it is beeing received:
   constexpr static int message_buffer_size{ 2048 };
-  char message_buffer[message_buffer_size];
-  int message_buffer_position{ 0 };
+  char m_message_buffer[message_buffer_size];
+  int m_message_buffer_position{ 0 };
 
   // Store the CRC part of the message as it is beeing received:
   constexpr static int crc_buffer_size{ 8 };
-  char crc_buffer[crc_buffer_size];
-  int crc_buffer_position{ 0 };
+  char m_crc_buffer[crc_buffer_size];
+  int m_crc_buffer_position{ 0 };
 
   // Calculate the CRC while the message is beeing received
-  uint16_t crc{ 0 };
+  uint16_t m_crc{ 0 };
+
+  // Keeps track of the start of the next line while processing.
+  char *m_start_of_line;
 
   enum class states {
     READING_MESSAGE,
@@ -69,20 +78,31 @@ private:
 
   void ChangeState(enum states new_state)
   {
+    unsigned long const current_time { millis() };
     switch (new_state) {
     case states::READING_MESSAGE:
+      m_reading_message_time = current_time;
+      m_num_message_loops = m_num_processing_loops = 0;
       m_CTS_switch->turn_on();
       if (m_status_switch != nullptr) m_status_switch->turn_on();
-      crc = message_buffer_position = 0;
+      m_crc = m_message_buffer_position = 0;
       break;
     case states::READING_CRC:
-      crc_buffer_position = 0;
+      m_reading_crc_time = current_time;
+      m_crc_buffer_position = 0;
       break;
     case states::PROCESSING:
+      m_processing_time = current_time;
       m_CTS_switch->turn_off();
+      m_start_of_line = m_message_buffer;
       break;
     case states::WAITING:
-      if (m_state != states::PROCESSING) m_CTS_switch->turn_off();
+      m_display_time_stats = true;
+      m_waiting_time = current_time;
+      if (m_state != states::PROCESSING) {
+        m_reading_crc_time = m_processing_time = current_time;
+        m_CTS_switch->turn_off();
+      }
       if (m_status_switch != nullptr) m_status_switch->turn_off();
       break;
     }
@@ -97,9 +117,9 @@ private:
   }
 
   class SensorListItem {
-    uint32_t m_obisCode;
+    uint32_t const m_obisCode;
     Sensor m_sensor;
-    SensorListItem *m_next{ nullptr };
+    SensorListItem * const m_next{ nullptr };
   public:
     SensorListItem(SensorListItem *next, uint32_t obisCode)
     : m_obisCode(obisCode)
@@ -107,8 +127,8 @@ private:
     {}
     
     Sensor *GetSensor() { return &m_sensor; }
-    uint32_t GetCode() { return m_obisCode; }
-    SensorListItem *Next() { return m_next; }
+    uint32_t GetCode() const { return m_obisCode; }
+    SensorListItem *Next() const { return m_next; }
   };
 
   // Linked list of all sensors
@@ -121,48 +141,51 @@ public:
 
   void setup() override 
   {
-    m_period_start_time = millis();
     ChangeState(states::READING_MESSAGE);
   }
 
   void loop() override {
+    unsigned long const loop_start_time { millis() };
     switch (m_state) {
     case states::READING_MESSAGE:
     case states::READING_CRC:
+      ++m_num_message_loops;
       while (available()) {
         // While data is available, read it one byte at a time.
         char const read_byte{ (char)read() }; 
         if (m_state == states::READING_MESSAGE) {
-          if (message_buffer_position == message_buffer_size) {
+          if (m_message_buffer_position == message_buffer_size) {
             ESP_LOGW("p1reader", "Message buffer overrun. Resetting.");
             ChangeState(states::WAITING);
-            while (available()) read();
+            int max_bytes_to_discard{ 1000 };
+            while (available() && max_bytes_to_discard-- != 0) read();
             return;
           }
           crc16_update(read_byte);
           if (read_byte == '!') {
             // The exclamation mark indicates that the main message is complete
             // and the CRC will come next.
-            message_buffer[message_buffer_position] = '\0';
+            m_message_buffer[m_message_buffer_position] = '\0';
             ChangeState(states::READING_CRC);
           }
-          else message_buffer[message_buffer_position++] = read_byte;
+          else m_message_buffer[m_message_buffer_position++] = read_byte;
         }
         else { // READING_CRC
-          if (crc_buffer_position == crc_buffer_size) {
+          if (m_crc_buffer_position == crc_buffer_size) {
             ESP_LOGW("p1reader", "CRC buffer overrun. Resetting.");
             ChangeState(states::WAITING);
-            while (available()) read();
+            int max_bytes_to_discard{ 1000 };
+            while (available() && max_bytes_to_discard-- != 0) read();
             return;
           }
           if (read_byte == '\n') {
             // The CRC is a single line, so once we reach end of line, we are
             // ready to verify and process the message.
-            crc_buffer[crc_buffer_position] = '\0';
-            int const crcFromMsg = (int) strtol(crc_buffer, NULL, 16);
-            if (crc != crcFromMsg) {
-              ESP_LOGW("p1reader", "CRC missmatch, calculated %04X != %04X. Message ignored.", crc, crcFromMsg);
-              ESP_LOGD("p1reader", "Buffer:\n%s", message_buffer);
+            m_crc_buffer[m_crc_buffer_position] = '\0';
+            int const crcFromMsg = (int) strtol(m_crc_buffer, NULL, 16);
+            if (m_crc != crcFromMsg) {
+              ESP_LOGW("p1reader", "CRC missmatch, calculated %04X != %04X. Message ignored.", m_crc, crcFromMsg);
+              ESP_LOGD("p1reader", "Buffer:\n%s", m_message_buffer);
               ChangeState(states::WAITING);
               return;
             }
@@ -172,19 +195,54 @@ public:
             }
           }
           else {
-            crc_buffer[crc_buffer_position++] = read_byte;
+            m_crc_buffer[m_crc_buffer_position++] = read_byte;
           }
         }
       }
       break;
     case states::PROCESSING:
-      ParseMessage();
-      ChangeState(states::WAITING);
+      ++m_num_processing_loops;
+      do {
+        while (*m_start_of_line == '\n' || *m_start_of_line == '\r') ++m_start_of_line;
+        char *end_of_line{ m_start_of_line };
+        while (*end_of_line != '\n' && *end_of_line != '\r' && *end_of_line != '\0') ++end_of_line;
+        bool const last_line{ *end_of_line == '\0' };
+        *end_of_line = '\0';
+  
+        if (end_of_line != m_start_of_line) {
+          int minor{ -1 }, major{ -1 }, micro{ -1 };
+          double value{ -1.0 };
+          if (sscanf(m_start_of_line, "1-0:%d.%d.%d(%lf", &major, &minor, &micro, &value) != 4) {
+            ESP_LOGD("p1reader", "Could not parse value from line '%s'", m_start_of_line);
+          }
+          else {
+            uint32_t const obisCode{ OBIS(major, minor, micro) };
+            Sensor *S{ GetSensor(obisCode) };
+            if (S != nullptr) S->publish_state(value);
+            else {
+              ESP_LOGD("p1reader", "No sensor matching: %d.%d.%d (0x%x)", major, minor, micro, obisCode);
+            }
+          }
+        } 
+        if (last_line) {
+          ChangeState(states::WAITING);
+          return;
+        }
+        m_start_of_line = end_of_line + 1;
+      } while (millis() - loop_start_time < 25);
       break;
     case states::WAITING:
-      unsigned long const current_time{ millis() };
-      if (m_minimum_period_ms < current_time - m_period_start_time) {
-        m_period_start_time = current_time;
+      if (m_display_time_stats) {
+        m_display_time_stats = false;
+        ESP_LOGD("p1reader", "Cycle times: Message = %d ms (%d loops), Processing = %d ms (%d loops), (Total = %d ms)", 
+                 m_processing_time - m_reading_message_time, 
+                 m_num_message_loops, 
+                 m_waiting_time - m_processing_time,
+                 m_num_processing_loops,
+                 m_waiting_time - m_reading_message_time
+                 );
+      }
+      if (m_minimum_period_ms < loop_start_time - m_reading_message_time) {
         ChangeState(states::READING_MESSAGE);
       }
       break;
@@ -194,12 +252,12 @@ public:
 private:
   void crc16_update(uint8_t a) {
     int i;
-    crc ^= a;
+    m_crc ^= a;
     for (i = 0; i < 8; ++i) {
-      if (crc & 1) {
-          crc = (crc >> 1) ^ 0xA001;
+      if (m_crc & 1) {
+          m_crc = (m_crc >> 1) ^ 0xA001;
       } else {
-          crc = (crc >> 1);
+          m_crc = (m_crc >> 1);
       }
     }
   }
@@ -216,40 +274,5 @@ private:
     }
     return nullptr;
   }
-
-  // Once a complete message has been received and CRC-verified, this method
-  // will process the contents.
-  void ParseMessage() {
-    char *start_of_line{ message_buffer };
-    while (*start_of_line == '\n' || *start_of_line == '\r') ++start_of_line;
-    
-    // Message will be processed one line at a time:
-    while (*start_of_line != '\0') {
-      char *end_of_line{ start_of_line };
-      while (*end_of_line != '\n' && *end_of_line != '\r' && *end_of_line != '\0') ++end_of_line;
-      bool const last_line{ *end_of_line == '\0' };
-      *end_of_line = '\0';
-
-      if (end_of_line != start_of_line) {
-        int minor{ -1 }, major{ -1 }, micro{ -1 };
-        double value{ -1.0 };
-        if (sscanf(start_of_line, "1-0:%d.%d.%d(%lf", &major, &minor, &micro, &value) != 4) {
-          ESP_LOGD("p1reader", "Could not parse value from line '%s'", start_of_line);
-        }
-        else {
-          uint32_t const obisCode{ OBIS(major, minor, micro) };
-          Sensor *S{ GetSensor(obisCode) };
-          if (S != nullptr) S->publish_state(value);
-          else {
-            ESP_LOGD("p1reader", "No sensor matching: %d.%d.%d (0x%x)", major, minor, micro, obisCode);
-          }
-        }
-      }
-      if (last_line) return;
-      start_of_line = end_of_line + 1;
-      while (*start_of_line == '\n' || *start_of_line == '\r') ++start_of_line;
-    }
-  }
-
 
 };
